a943c9977f0ad46aaab4759a814c567f9c547fbb
Jorge Ricardo Soares <jorgersoares@outlook.com>
2021-09-28 06:07:09 -0300

    cors config

--- src/main/java/br/edu/ifpb/api/application/controller/SmsController.java
+++ src/main/java/br/edu/ifpb/api/application/controller/SmsController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.edu.ifpb.api.presentation.dto.SmsRequest;
 
 @RestController
 @RequestMapping("/sms")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SmsController {
 
     @Autowired
