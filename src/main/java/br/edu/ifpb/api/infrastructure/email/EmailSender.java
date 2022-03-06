e56bedce8dec1803dcd9705e98c1a1e17a21aab7
Jorge Ricardo Soares <30946860+Jorgersoares@users.noreply.github.com>
2022-03-06 12:03:57 -0300

    Update EmailSender.java

--- src/main/java/br/edu/ifpb/api/infrastructure/email/EmailSender.java
+++ src/main/java/br/edu/ifpb/api/infrastructure/email/EmailSender.java
@@ -14,7 +14,7 @@ public class EmailSender {
     public void sendEmail(String email, String body, String subject) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(email);
-        msg.setFrom("sejide1225@decorbuz.com");
+        msg.setFrom("");
         msg.setSubject(subject);
         msg.setText(body);
 
